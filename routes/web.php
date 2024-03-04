10ba97c81d9f75b3eb588c1ae4af8d36d947a82c
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-04 11:02:47 +0200

    create routes for todo controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\TodoController;
+use app\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +14,20 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+//Todo Controller Routes
+
+Route::post('/todos', TodoController::class, 'createTodo');
+Route::get('/todos/{$id}', TodoController::class, 'displayTodo');
+Route::patch('/todos/{$id}', TodoController::class, 'updateTodo');
+Route::delete('/todos/{$id}', TodoController::class, 'deleteTodo');
+
+// // Task Controller Routes
+// Route::post('/tasks', TaskController::class, 'createTask');
+// Route::get('/tasks/{$id}', TaskController::class, 'displayTask');
+// Route::patch('/tasks/{$id}', TaskController::class, 'updateTask');
+// Route::delete('/tasks/{$id}', TaskController::class, 'deleteTask');
+
 
 Route::get('/', function () {
     return view('welcome');
